62eaef7454cced0adce0d32d6e2facf15cf089d6
gabor <info@gfarkas.com>
2022-03-16 21:06:48 +0100

    - slice buffers

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
-var buffer1 = new Buffer('ABC');
-var buffer2 = new Buffer('ABCD');
-var result = buffer1.compare(buffer2);
+var buffer1 = new Buffer('TutorialsPoint');
 
-if(result < 0) {
-    console.log(buffer1 +" comes before " + buffer2);
-} else if(result === 0) {
-    console.log(buffer1 +" is same as " + buffer2);
-} else {
-    console.log(buffer1 +" comes after " + buffer2);
-}
+//slicing a buffer
+var buffer2 = buffer1.slice(0,9);
+console.log("buffer2 content: " + buffer2.toString());
