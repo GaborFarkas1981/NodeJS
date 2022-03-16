85dc7e78e92817280c2b896f9b1fe1bc604b056b
gabor <info@gfarkas.com>
2022-03-16 21:17:11 +0100

    - piping the streams

--- main.js
+++ main.js
@@ -1,22 +1,13 @@
 const fs = require("fs");
-const data = 'Simply Easy Learning';
+
+// Create a readable stream
+const readerStream = fs.createReadStream('input.txt');
 
 // Create a writable stream
 const writerStream = fs.createWriteStream('output.txt');
 
-// Write the data to stream with encoding to be utf8
-writerStream.write(data,'UTF8');
-
-// Mark the end of file
-writerStream.end();
-
-// Handle stream events --> finish, and error
-writerStream.on('finish', function() {
-    console.log("Write completed.");
-});
-
-writerStream.on('error', function(err) {
-    console.log(err.stack);
-});
+// Pipe the read and write operations
+// read input.txt and write data to output.txt
+readerStream.pipe(writerStream);
 
 console.log("Program Ended");
