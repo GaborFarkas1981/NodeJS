359c91b1beaf788e31d5eb96e0417f5e8bb4869e
gabor <info@gfarkas.com>
2022-03-16 21:10:57 +0100

    - stream

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-var buffer1 = new Buffer('TutorialsPoint');
+var fs = require("fs");
+var data = '';
 
-//slicing a buffer
-var buffer2 = buffer1.slice(0,9);
-console.log("buffer2 content: " + buffer2.toString());
+// Create a readable stream
+var readerStream = fs.createReadStream('input.txt');
+
+// Set the encoding to be utf8.
+readerStream.setEncoding('UTF8');
+
+// Handle stream events --> data, end, and error
+readerStream.on('data', function(chunk) {
+    data += chunk;
+});
+
+readerStream.on('end',function() {
+    console.log(data);
+});
+
+readerStream.on('error', function(err) {
+    console.log(err.stack);
+});
+
+console.log("Program Ended");
